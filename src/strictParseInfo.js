b6e2fb95ef1464b824f4fddfe38c8ed4ae13e6c0
ashish mahindrakar <ashishmahi46@gmail.com>
2018-01-08 15:46:35 +0530

    added feature: case-sensitive  parsing as per requirement

--- src/strictParseInfo.js
+++ src/strictParseInfo.js
@@ -1,22 +1,32 @@
 const Parsed=require("./parsed.js");
 const ParseInfo=require("./parseInfo.js");
+const errors=function(filePath){return "../src/errors/"+filePath};
 const InvalidKeyError=require("./errors/invalidKeyError.js");
 
-const contains=function(list,key) {
+const contains=function(list,key,caseSensitiveCheck) {
   return list.find(function(validKey){
-    return key==validKey;
+    if(caseSensitiveCheck){
+      return key == validKey;
+    }
+    return isStrictEqual(key,validKey);
   });
 }
 
-var StrictParseInfo=function(initialParsingFunction,validKeys) {
+const isStrictEqual = function(key,validKey){
+  return key.toLowerCase()==validKey.toLowerCase();
+}
+
+var StrictParseInfo=function(initialParsingFunction,validKeys,isCaseSensitive) {
   ParseInfo.call(this,initialParsingFunction);
   this.validKeys=validKeys;
+  this.caseSensitiveCheck = isCaseSensitive;
 }
 
 StrictParseInfo.prototype=Object.create(ParseInfo.prototype);
 
 StrictParseInfo.prototype.pushKeyValuePair=function() {
-  if(!contains(this.validKeys,this.currentKey))
+  let caseSensitiveCheck = this.caseSensitiveCheck;
+  if(!contains(this.validKeys,this.currentKey,caseSensitiveCheck))
     throw new InvalidKeyError("invalid key",this.currentKey,this.currentPos);
   this.parsedKeys[this.currentKey]=this.currentValue;
   this.resetKeysAndValues();
--- test/testCaseSensitiveStrictParser.js
+++ test/testCaseSensitiveStrictParser.js
@@ -1,8 +1,18 @@
 const src=function(filePath){return "../src/"+filePath};
-
+const errors=function(filePath){return "../src/errors/"+filePath};
+const InvalidKeyError=require(errors('invalidKeyError.js'));
 const assert=require('chai').assert;
 const Parsed=require(src('parsed.js'));
 const StrictParser=require(src('index.js')).StrictParser;
+
+var invalidKeyErrorChecker=function(key,pos) {
+  return function(err) {
+    if(err instanceof InvalidKeyError && err.invalidKey==key && err.position==pos){
+      return true;
+    }
+    return false;
+  }
+};
 
 describe("strict parser that is case insensitive",function(){
   it("should parse when specified keys are in lower case and actual is not",function(){
@@ -12,5 +22,18 @@ describe("strict parser that is case insensitive",function(){
     expected["NAME"]="jayanth";
     let parsed=kvParser.parse("NAME=jayanth");
     assert.deepEqual(parsed,expected);
+  });
+
+  it("should throw error when specified keys are in lower case and actual is not",function(){
+    let kvParser=new StrictParser(["name"],true);
+    assert.throws(
+      () => {
+        try {
+          var p=kvParser.parse("NAME=JAYANT");
+        } catch (e) {
+          if(invalidKeyErrorChecker("NAME",10)(e)) throw e;
+        }
+      },
+      Error,"invalid key");
   });
 });
